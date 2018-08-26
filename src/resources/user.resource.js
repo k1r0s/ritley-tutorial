94c19165e6bcce31adad4aa268d301194c5e3fc2
k1r0s <ciroreed@gmail.com>
2018-08-26 16:21:09 +0200

    handling exceptions

--- src/resources/user.resource.js
+++ src/resources/user.resource.js
@@ -1,7 +1,16 @@
 import { AbstractResource } from "@ritley/core";
-import UserModel from "../models/user.model";
+import UserModel, { UserValidationError, UserMailInUseError } from "../models/user.model";
 
-import { Dependency, ReqTransformBodyAsync } from "@ritley/decorators";
+import {
+  Dependency,
+  ReqTransformBodyAsync,
+  Default,
+  Throws,
+  InternalServerError,
+  BadRequest,
+  Conflict,
+  Created
+} from "@ritley/decorators";
 
 @Dependency("userModel", UserModel)
 export default class UserResource extends AbstractResource {
@@ -9,14 +18,16 @@ export default class UserResource extends AbstractResource {
     super("/users");
   }
 
+  @Throws(SyntaxError, BadRequest)
+  @Throws(UserValidationError, BadRequest)
+  @Throws(UserMailInUseError, Conflict)
+  @Default(Created)
   @ReqTransformBodyAsync
   async post(req, res) {
     const body = await req.body;
     const payload = body.toJSON();
     await this.userModel.validate(payload);
     await this.userModel.isUnique(payload);
-    const user = await this.userModel.create(payload);
-    res.statusCode = 200;
-    res.end(JSON.stringify(user));
+    return this.userModel.create(payload);
   }
 }
